6c8c6b572b19d99d1cd80e1d4d1b88552e70a256
Paula McKinley <pmknyc@optimum.net>
2018-09-28 21:48:46 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
+require 'pry'
+
 def unsafe?(speed)
-
+	if speed.between?(40,60)
+		false
+	else
+		true
+	end
 end
-
-
 
 def not_safe?(speed)
-	
+	(speed < 40) | (speed > 60) ? true : false
 end
-	
-
-
